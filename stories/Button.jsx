77533d5979a63d9df3e9fa1ad846c8c1c35844a1
Sebastian Nemeth <sebastian@cleric.sh>
2023-08-03 12:47:07 +0300

    add 'isomorphic-unfetch', repro error in Button.jsx

--- stories/Button.jsx
+++ stories/Button.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
+
+import unfetch from 'isomorphic-unfetch';
+
+unfetch;
 
 /**
  * Primary UI component for user interaction
